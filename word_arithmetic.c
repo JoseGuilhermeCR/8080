2fe2be6146dcf6cec6f42b647ff6b60807cb2522
JoseGuilhermeCR <joseguilhermebh@hotmail.com>
2020-04-04 22:51:05 -0300

    File changes

--- word_arithmetic.c
+++ word_arithmetic.c
@@ -1,5 +1,61 @@
 #include "i8080emu.h"
 
+/* Increment Word Instructions */
+
+/* Macro for defining inx instructions */
+#define INX_INSTR(r,R,R1)	\
+	INSTR(inx_##r) {	\
+		uint16_t register_pair = ((emu->i8080.R << 8) | emu->i8080.R1) + 1;	\
+		emu->i8080.R1 = LB(register_pair);	\
+		emu->i8080.R  = HB(register_pair);	\
+	}
+
+INX_INSTR(b,B,C)
+INX_INSTR(d,D,E)
+INX_INSTR(h,H,L)
+
+/*
+Example with comments:
+INSTR(inx_d) {
+	// Increment register pair DE
+	uint16_t register_pair = ((emu->i8080.D << 8) | emu->i8080.E) + 1;
+	emu->i8080.D = LB(register_pair);
+	emu->i8080.E = HB(register_pair);
+}
+*/
+
+INSTR(inx_sp) {
+	++emu->i8080.SP;
+}
+
+/* Decrement Word Instructions */
+
+/* Macro for defining dcx instructions */
+#define DCX_INSTR(r,R,R1)	\
+	INSTR(dcx_##r) {	\
+		uint16_t register_pair = ((emu->i8080.R << 8) | emu->i8080.R1) - 1;	\
+		emu->i8080.R1 = LB(register_pair);	\
+		emu->i8080.R  = HB(register_pair);	\
+	}
+
+DCX_INSTR(b,B,C)
+DCX_INSTR(d,D,E)
+DCX_INSTR(h,H,L)
+
+/*
+Example with comments:
+INSTR(dcx_b) {
+	// Decrement register pair BC.
+	uint16_t register_pair = ((emu->i8080.B << 8) | emu->i8080.C) - 1;
+	emu->i8080.C = LB(register_pair);
+	emu->i8080.B = HB(register_pair);
+}
+*/
+
+INSTR(dcx_sp) {
+	--emu->i8080.SP;
+}
+
 /* Double Byte Add Instructions */
 
 /* Macro for defining dad instructions */
